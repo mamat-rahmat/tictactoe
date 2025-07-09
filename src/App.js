ad71553b2ae519d947b31fb826918ca3ca249102
Mamat Rahmat <coderbodoh@gmail.com>
2025-07-09 15:11:25 +0700

    X and O moves alternately

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-function Square({ value }) {
+import { useState } from 'react';
+
+
+function Square({ value, onClick }) {
   return (
-    <button className="square">
+    <button className="square" onClick={onClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [isXNext, setIsXNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    if (squares[i]) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[i] = isXNext ? 'X' : 'O';
+    setIsXNext(!isXNext);
+    setSquares(newSquares);
+  };
+
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </>
   )
